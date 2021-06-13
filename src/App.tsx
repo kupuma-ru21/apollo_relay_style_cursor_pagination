0d601e38f138edce40158861296401e6a2970381
kupuma-ru21 <kupumaru21@gmail.com>
2021-06-13 17:49:41 +0900

    feat: next page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@apollo/client';
+import { useCallback } from 'react';
 import { REPOSITORIES } from './graphql';
 export const PER_PAGE = 5;
 export const VARIABLES = {
@@ -10,19 +11,37 @@ export const VARIABLES = {
 };
 
 const App = () => {
-  const { data, loading, error } = useQuery(REPOSITORIES, {
+  const { data, loading, error, fetchMore } = useQuery(REPOSITORIES, {
     variables: VARIABLES,
   });
+  const nextPage = useCallback(
+    (pageInfo) => {
+      fetchMore({
+        variables: {
+          first: null,
+          before: null,
+          last: 5,
+          after: pageInfo.endCursor,
+        },
+      });
+    },
+    [fetchMore]
+  );
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
   const repositoriesInfo = data.search.edges;
+  const pageInfo = data.search.pageInfo;
   return (
-    <ul>
-      {repositoriesInfo.map((repository: any) => {
-        const { name } = repository.node;
-        return <li key={name}>{name}</li>;
-      })}
-    </ul>
+    <div>
+      <ul>
+        {repositoriesInfo.map((repository: any) => {
+          const { name } = repository.node;
+          return <li key={name}>{name}</li>;
+        })}
+      </ul>
+      <button onClick={() => nextPage(pageInfo)}>nextPage</button>
+    </div>
   );
 };
 
